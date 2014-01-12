00526b4ae7e93c11f39da78c41ebb0b6e46df763
Justin Liu <yuting1987@gmail.com>
2014-01-13 01:51:00 +0800

    Support mongodb

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function(){
+  setTimeout(function(){
+    $('#hashtag_permalink_feed .genericStreamStory').each(function(i,item){
+      var fbid = /posts\/([0-9]+)/.exec($(item).html());
+      $.post('http://localhost:8080/api/push', {
+        name: $('.actorName', item).text(),
+        message: $('.userContent', item).text(),
+        link: 'https://www.facebook.com/'+fbid[1]
+      });
+    });  
+  }, 2000);
+});
